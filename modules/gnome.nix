161295a57ffd8fee7f6f9c9bb29df827e56cdc04
Charan Vadapalli <sree.saicharan.vadapalli@gmail.com>
2026-07-10 21:14:50 +0530

    feat: add gnome module

---
+++ modules/gnome.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+{
+
+  # Enable X11
+  services.xserver.enable = true; 
+
+  # GNOME Desktop Env
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  
+  # Disable CUPS to not print documents, also what even is CUPS?
+  services.printing.enable = false;
+
+
+  # Disable bloat
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-maps
+    gnome-music
+    gnome-connections
+    gnome-characters
+    gnome-weather
+    gnome-contacts
+    gnome-calendar
+
+    epiphany
+    simple-scan
+  ];
+
+
+  environment.systemPackages = with pkgs; [
+  ];
+}
+
+
